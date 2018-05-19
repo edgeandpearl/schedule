3ff9dd155699a6b16d5d561b9e3e260b618539c7
edgeandpearl <marinakoustova@gmail.com>
2018-05-19 20:02:15 +0300

    add stuff rm other stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './less/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './less/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Page extends React.Component {
+
+  render () {
+    return (
+      <div class="kek">
+        sup
+        <span class="kek_us">supsup</span>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('dev')
+);
